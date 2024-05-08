0ce9d0776e15f5cdcad78ddd246a37540543ef28
[cj-2123] <cj2123acct@gmail.com>
2024-05-07 19:15:48 -0700

    scroll wheel functionality

--- src/components/Tracker.jsx
+++ src/components/Tracker.jsx
@@ -3,7 +3,6 @@ import items from "../data/items.js";
 
 function Tracker() {
   const [imageStyles, setImageStyles] = useState({});
-  const [tooltipId, setTooltipId] = useState(null);
   const [iconCounts, setIconCounts] = useState({});
 
   // Initialize item counts and styles
@@ -20,13 +19,9 @@ function Tracker() {
     setIconCounts(initialCounts);
   }, []);
 
-  // Toggle styling of items and incrementing/decrementing items in the tracker when clicked
-  function handleOpacityToggle(event) {
-    const { id } = event.target;
-    const isLeftClick = event.button === 0; // left click
-    const isRightClick = event.button === 2; // right click
-
-    if (isLeftClick) {
+  // Toggle styling of items and incrementing/decrementing items in the tracker when clicked/scrolled
+  function handleOpacityToggle(id, increment) {
+    if (increment) {
       if (iconCounts[id] < items.find((item) => item.id === id).max) {
         // Check if count is less than max
         setImageStyles((prevStyles) => ({
@@ -41,7 +36,7 @@ function Tracker() {
           [id]: prevCounts[id] + 1,
         }));
       }
-    } else if (isRightClick && iconCounts[id] > 1) {
+    } else if (!increment && iconCounts[id] > 1) {
       // Check is count greater than 1 on decrement
       setImageStyles((prevStyles) => ({
         ...prevStyles,
@@ -54,8 +49,7 @@ function Tracker() {
         ...prevCounts,
         [id]: prevCounts[id] - 1,
       }));
-      event.preventDefault(); // Prevent the context menu from appearing
-    } else if (isRightClick && iconCounts[id] === 1) {
+    } else if (!increment && iconCounts[id] === 1) {
       // Fade image if count is 1 and decrementing
       setImageStyles((prevStyles) => ({
         ...prevStyles,
@@ -68,7 +62,6 @@ function Tracker() {
         ...prevCounts,
         [id]: prevCounts[id] - 1,
       }));
-      event.preventDefault(); // Prevent the context menu from appearing
     }
   }
 
@@ -76,6 +69,20 @@ function Tracker() {
   const rows = [];
   for (let i = 0; i < items.length; i += 6) {
     rows.push(items.slice(i, i + 6));
+  }
+
+  // Get mouse wheel scrolling value
+  function handleWheel(event, id) {
+    const increment = event.deltaY < 0; // Scroll up
+    handleOpacityToggle(id, increment);
+    //event.preventDefault(); // Prevent default scrolling behavior
+  }
+
+  // Get mouse click value
+  function handleClick(event, id) {
+    const increment = event.button === 0; // left click
+    //const isRightClick = event.button === 2; // right click
+    handleOpacityToggle(id, increment);
   }
 
   // Create item tracker
@@ -92,10 +99,9 @@ function Tracker() {
                       id={id}
                       style={imageStyles[id]}
                       src={src}
-                      onMouseDown={handleOpacityToggle}
+                      onMouseDown={(e) => handleClick(e, id)}
+                      onWheel={(e) => handleWheel(e, id)}
                       onContextMenu={(e) => e.preventDefault()}
-                      onMouseEnter={() => setTooltipId(id)}
-                      onMouseLeave={() => setTooltipId(null)}
                     />
                     {items.find((item) => item.id === id).max > 1 && (
                       <span className="count tracker-count">
